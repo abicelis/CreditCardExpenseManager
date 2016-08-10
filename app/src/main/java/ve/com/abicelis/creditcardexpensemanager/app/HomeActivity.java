0637724f80f4cb1da5069379d2e485bb7fa423e0
Alejandro Bicelis <abicelis@gmail.com>
2016-08-10 18:16:59 -0400

    HomeActivity improvements, fab calls createExpenseDialog, dialog actually works, other misc things

--- app/src/main/java/ve/com/abicelis/creditcardexpensemanager/app/HomeActivity.java
+++ app/src/main/java/ve/com/abicelis/creditcardexpensemanager/app/HomeActivity.java
@@ -1,80 +1,158 @@
 package ve.com.abicelis.creditcardexpensemanager.app;
 
+import android.content.DialogInterface;
 import android.os.Handler;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
 import java.util.List;
 
 import ve.com.abicelis.creditcardexpensemanager.R;
-import ve.com.abicelis.creditcardexpensemanager.adapter.ExpensesAdapter;
+import ve.com.abicelis.creditcardexpensemanager.app.adapter.ExpensesAdapter;
+import ve.com.abicelis.creditcardexpensemanager.app.dialogs.CreateExpenseDialogFragment;
 import ve.com.abicelis.creditcardexpensemanager.database.ExpenseManagerDAO;
 import ve.com.abicelis.creditcardexpensemanager.exceptions.CouldNotInsertDataException;
 import ve.com.abicelis.creditcardexpensemanager.mocks.CreditMock;
 import ve.com.abicelis.creditcardexpensemanager.model.CreditCard;
+import ve.com.abicelis.creditcardexpensemanager.model.CreditPeriod;
+import ve.com.abicelis.creditcardexpensemanager.model.Expense;
+import ve.com.abicelis.creditcardexpensemanager.model.Payment;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener, DialogInterface.OnDismissListener {
 
     //Data
-    CreditCard creditCard;
+    List<CreditCard> creditCards;
+    List<CreditPeriod> creditPeriods;
+    List<Expense> expenses;
+    List<Payment> payments;
     ExpenseManagerDAO dao;
 
     //UI
     RecyclerView recyclerViewExpenses;
+    ExpensesAdapter adapter;
     Toolbar toolbar;
+    FloatingActionButton fabNewExpense;
+    FloatingActionButton fabNewExpenseCamera;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        creditCard = CreditMock.getCreditCardMock();
-
-        setUpExpensesRecyclerView();
-
-        setUpToolbar();
-
-
-
 
 
         Handler handler = new Handler();
-
         final Runnable r = new Runnable() {
             public void run() {
-                dao = new ExpenseManagerDAO(getApplicationContext());
-
-                try {
-                    dao.insertCreditCard(creditCard);
-                }catch (CouldNotInsertDataException e) {
-                    e.printStackTrace();
-                }
-
-                List<CreditCard> creditCards = dao.getCreditCardList();
-
-                System.out.println(creditCards.get(0).getCardAlias());
+                refreshData();
+                setUpExpensesRecyclerView();
             }
         };
         handler.postDelayed(r, 1000);
+
+
+
+        //setUpExpensesRecyclerView();
+        setUpToolbar();
+        setUpFab();
     }
 
-    private void setUpToolbar() {
-        toolbar = (Toolbar) findViewById(R.id.home_toolbar);
-        toolbar.setTitle("CC Expense Mgr");
+    private void refreshData() {
+        if(dao == null)
+            dao = new ExpenseManagerDAO(getApplicationContext());
+
+        creditCards = dao.getCreditCardList();
+        creditPeriods = dao.getCreditPeriodListFromCard(creditCards.get(0).getId());
+        expenses = dao.getExpensesFromCreditPeriod(creditPeriods.get(0).getId());
+        payments = dao.getPaymentsFromCreditPeriod(creditPeriods.get(0).getId());
     }
+
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.home_fab_new_expense:
+                showCreateExpenseDialog();
+                break;
+            case R.id.home_fab_new_expense_camera:
+                Toast.makeText(HomeActivity.this, "Not yet implemented!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+
+    private void showCreateExpenseDialog() {
+        FragmentManager fm = getSupportFragmentManager();
+        CreateExpenseDialogFragment dialog = CreateExpenseDialogFragment.newInstance(getResources().getString(R.string.dialog_create_expense_title), dao);
+        dialog.show(fm, "fragment_dialog_create_expense");
+    }
+
+
+
+
 
     private void setUpExpensesRecyclerView() {
 
-
         recyclerViewExpenses = (RecyclerView) findViewById(R.id.home_recycler_expenses);
 
-        ExpensesAdapter adapter = new ExpensesAdapter(getApplicationContext(), creditCard.getCreditPeriods().get(0).getExpenses());
+        adapter = new ExpensesAdapter(getApplicationContext(), expenses);
         recyclerViewExpenses.setAdapter(adapter);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
         recyclerViewExpenses.setLayoutManager(layoutManager);
+    }
+
+    private void setUpToolbar() {
+
+        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.home_collapsing);
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.home_appbar);
+        toolbar = (Toolbar) findViewById(R.id.home_toolbar);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.setTitle(getResources().getString(R.string.app_name));
+                    isShow = true;
+                } else if(isShow) {
+                    collapsingToolbarLayout.setTitle(" ");//carefull there should a space between double quote otherwise it wont work
+
+                    isShow = false;
+                }
+            }
+        });
+    }
+
+    private void setUpFab() {
+        fabNewExpense = (FloatingActionButton) findViewById(R.id.home_fab_new_expense);
+        fabNewExpenseCamera = (FloatingActionButton) findViewById(R.id.home_fab_new_expense_camera);
+
+        fabNewExpense.setOnClickListener(this);
+        fabNewExpenseCamera.setOnClickListener(this);
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialogInterface) {
+        refreshData();
+        adapter.notifyDataSetChanged();
     }
 }
