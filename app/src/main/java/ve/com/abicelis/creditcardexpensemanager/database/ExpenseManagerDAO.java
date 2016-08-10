fcdac865f176602f48165a611260bdf89cbaf5fb
Alejandro Bicelis <abicelis@gmail.com>
2016-08-10 18:14:56 -0400

    new getters in db dao, getPeriodList, getExpensesForPeriod, getpaymentsForPeriod + helper methods to convert cursor data obtained from db

--- app/src/main/java/ve/com/abicelis/creditcardexpensemanager/database/ExpenseManagerDAO.java
+++ app/src/main/java/ve/com/abicelis/creditcardexpensemanager/database/ExpenseManagerDAO.java
@@ -5,14 +5,18 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 import ve.com.abicelis.creditcardexpensemanager.enums.CreditCardType;
 import ve.com.abicelis.creditcardexpensemanager.enums.Currency;
+import ve.com.abicelis.creditcardexpensemanager.enums.ExpenseCategory;
+import ve.com.abicelis.creditcardexpensemanager.enums.ExpenseType;
 import ve.com.abicelis.creditcardexpensemanager.exceptions.CouldNotInsertDataException;
 import ve.com.abicelis.creditcardexpensemanager.model.CreditCard;
+import ve.com.abicelis.creditcardexpensemanager.model.CreditPeriod;
 import ve.com.abicelis.creditcardexpensemanager.model.Expense;
 import ve.com.abicelis.creditcardexpensemanager.model.Payment;
 
@@ -60,18 +64,7 @@ public class ExpenseManagerDAO {
 
         try {
             while (cursor.moveToNext()) {
-                int id = cursor.getInt(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable._ID));
-                String cardAlias = cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_CARD_ALIAS.getName()));
-                String bankName = cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_BANK_NAME.getName()));
-                String cardNumber = cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_CARD_NUMBER.getName()));
-                Currency currency = Currency.valueOf(cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_CURRENCY.getName())));
-                CreditCardType cardType = CreditCardType.valueOf(cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_CARD_TYPE.getName())));
-                Calendar cardExpiration = Calendar.getInstance();
-                cardExpiration.setTimeInMillis(cursor.getLong(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_CARD_EXPIRATION.getName())));
-                int closingDay = cursor.getInt(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_CLOSING_DAY.getName()));
-                int dueDay = cursor.getInt(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_DUE_DAY.getName()));
-
-                creditCards.add(new CreditCard(id, cardAlias, bankName, cardNumber, currency, cardType, cardExpiration, closingDay, dueDay));
+                creditCards.add(getCreditCardFromCursor(cursor));
             }
         } finally {
             cursor.close();
@@ -79,6 +72,62 @@ public class ExpenseManagerDAO {
 
         return creditCards;
     }
+
+    public List<CreditPeriod> getCreditPeriodListFromCard(int cardId) {
+        List<CreditPeriod> creditPeriods = new ArrayList<>();
+        SQLiteDatabase db = mDatabaseHelper.getReadableDatabase();
+
+        Cursor cursor =  db.query(ExpenseManagerContract.CreditPeriodTable.TABLE_NAME, null,
+                ExpenseManagerContract.CreditPeriodTable.COLUMN_NAME_FOREIGN_KEY_CREDIT_CARD.getName() + " == " + cardId, null, null, null, null);
+
+        try {
+            while (cursor.moveToNext()) {
+                creditPeriods.add(getCreditPeriodFromCursor(cursor));
+            }
+        } finally {
+            cursor.close();
+        }
+
+        return creditPeriods;
+    }
+
+    public List<Expense> getExpensesFromCreditPeriod(int periodId) {
+        List<Expense> expenses = new ArrayList<>();
+        SQLiteDatabase db = mDatabaseHelper.getReadableDatabase();
+
+        Cursor cursor =  db.query(ExpenseManagerContract.ExpenseTable.TABLE_NAME, null,
+                ExpenseManagerContract.ExpenseTable.COLUMN_NAME_FOREIGN_KEY_CREDIT_PERIOD.getName() + " == " + periodId, null, null, null, null);
+
+        try {
+            while (cursor.moveToNext()) {
+                expenses.add(getExpenseFromCursor(cursor));
+            }
+        } finally {
+            cursor.close();
+        }
+
+        return expenses;
+    }
+
+    public List<Payment> getPaymentsFromCreditPeriod(int periodId) {
+        List<Payment> payments = new ArrayList<>();
+        SQLiteDatabase db = mDatabaseHelper.getReadableDatabase();
+
+        Cursor cursor =  db.query(ExpenseManagerContract.PaymentTable.TABLE_NAME, null,
+                ExpenseManagerContract.PaymentTable.COLUMN_NAME_FOREIGN_KEY_CREDIT_PERIOD.getName() + " == " + periodId, null, null, null, null);
+
+        try {
+            while (cursor.moveToNext()) {
+                payments.add(getPaymentFromCursor(cursor));
+            }
+        } finally {
+            cursor.close();
+        }
+
+        return payments;
+    }
+
+
 
 
     public long insertExpense(int creditPeriodId, Expense expense) throws CouldNotInsertDataException {
@@ -123,5 +172,67 @@ public class ExpenseManagerDAO {
             throw new CouldNotInsertDataException("There was a problem inserting the Payment: " + payment.toString());
 
         return newRowId;
+    }
+
+
+
+
+
+
+    private CreditCard getCreditCardFromCursor(Cursor cursor) {
+        int id = cursor.getInt(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable._ID));
+        String cardAlias = cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_CARD_ALIAS.getName()));
+        String bankName = cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_BANK_NAME.getName()));
+        String cardNumber = cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_CARD_NUMBER.getName()));
+        Currency currency = Currency.valueOf(cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_CURRENCY.getName())));
+        CreditCardType cardType = CreditCardType.valueOf(cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_CARD_TYPE.getName())));
+        Calendar cardExpiration = Calendar.getInstance();
+        cardExpiration.setTimeInMillis(cursor.getLong(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_CARD_EXPIRATION.getName())));
+        int closingDay = cursor.getInt(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_CLOSING_DAY.getName()));
+        int dueDay = cursor.getInt(cursor.getColumnIndex(ExpenseManagerContract.CreditCardTable.COLUMN_NAME_DUE_DAY.getName()));
+
+        return new CreditCard(id, cardAlias, bankName, cardNumber, currency, cardType, cardExpiration, closingDay, dueDay);
+    }
+
+
+    private CreditPeriod getCreditPeriodFromCursor(Cursor cursor) {
+        int id = cursor.getInt(cursor.getColumnIndex(ExpenseManagerContract.CreditPeriodTable._ID));
+        int periodNameStyle = cursor.getInt(cursor.getColumnIndex(ExpenseManagerContract.CreditPeriodTable.COLUMN_NAME_PERIOD_NAME_STYLE.getName()));
+        Calendar startDate = Calendar.getInstance();
+        startDate.setTimeInMillis(cursor.getLong(cursor.getColumnIndex(ExpenseManagerContract.CreditPeriodTable.COLUMN_NAME_START_DATE.getName())));
+        Calendar endDate = Calendar.getInstance();
+        endDate.setTimeInMillis(cursor.getLong(cursor.getColumnIndex(ExpenseManagerContract.CreditPeriodTable.COLUMN_NAME_END_DATE.getName())));
+        BigDecimal creditLimit =  new BigDecimal(cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.CreditPeriodTable.COLUMN_NAME_CREDIT_LIMIT.getName())));
+
+        return new CreditPeriod(id, periodNameStyle, startDate, endDate, creditLimit);
+    }
+
+
+    private Expense getExpenseFromCursor(Cursor cursor) {
+
+        int id = cursor.getInt(cursor.getColumnIndex(ExpenseManagerContract.ExpenseTable._ID));
+        String description = cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_DESCRIPTION.getName()));
+        byte[] image = cursor.getBlob(cursor.getColumnIndex(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_IMAGE.getName()));
+        BigDecimal amount = new BigDecimal(cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_AMOUNT.getName())));
+        Currency currency = Currency.valueOf(cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_CURRENCY.getName())));
+        Calendar date = Calendar.getInstance();
+        date.setTimeInMillis(cursor.getLong(cursor.getColumnIndex(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_DATE.getName())));
+        ExpenseCategory expenseCategory = ExpenseCategory.valueOf(cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_EXPENSE_CATEGORY.getName())));
+        ExpenseType expenseType = ExpenseType.valueOf(cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_EXPENSE_TYPE.getName())));
+
+        return new Expense(id, description, image, amount, currency, date, expenseCategory, expenseType);
+    }
+
+
+    private Payment getPaymentFromCursor(Cursor cursor) {
+
+        int id = cursor.getInt(cursor.getColumnIndex(ExpenseManagerContract.PaymentTable._ID));
+        String description = cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.PaymentTable.COLUMN_NAME_DESCRIPTION.getName()));
+        BigDecimal amount = new BigDecimal(cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.PaymentTable.COLUMN_NAME_AMOUNT.getName())));
+        Currency currency = Currency.valueOf(cursor.getString(cursor.getColumnIndex(ExpenseManagerContract.PaymentTable.COLUMN_NAME_CURRENCY.getName())));
+        Calendar date = Calendar.getInstance();
+        date.setTimeInMillis(cursor.getLong(cursor.getColumnIndex(ExpenseManagerContract.PaymentTable.COLUMN_NAME_DATE.getName())));
+
+        return new Payment(id, description, amount, currency, date);
     }
 }
