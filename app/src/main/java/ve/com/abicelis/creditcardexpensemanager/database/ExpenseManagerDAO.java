982d0b3d917e64f92ed423e27c16b1370635aec8
Alejandro Bicelis <abicelis@gmail.com>
2016-08-10 12:25:46 -0400

    New iinsert expense and  insert payment methods

--- app/src/main/java/ve/com/abicelis/creditcardexpensemanager/database/ExpenseManagerDAO.java
+++ app/src/main/java/ve/com/abicelis/creditcardexpensemanager/database/ExpenseManagerDAO.java
@@ -13,6 +13,8 @@ import ve.com.abicelis.creditcardexpensemanager.enums.CreditCardType;
 import ve.com.abicelis.creditcardexpensemanager.enums.Currency;
 import ve.com.abicelis.creditcardexpensemanager.exceptions.CouldNotInsertDataException;
 import ve.com.abicelis.creditcardexpensemanager.model.CreditCard;
+import ve.com.abicelis.creditcardexpensemanager.model.Expense;
+import ve.com.abicelis.creditcardexpensemanager.model.Payment;
 
 /**
  * Created by Alex on 8/8/2016.
@@ -26,7 +28,7 @@ public class ExpenseManagerDAO {
 
     }
 
-    public void insertCreditCard(CreditCard creditcard) throws CouldNotInsertDataException {
+    public long insertCreditCard(CreditCard creditcard) throws CouldNotInsertDataException {
         SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -44,6 +46,8 @@ public class ExpenseManagerDAO {
 
         if(newRowId == -1)
             throw new CouldNotInsertDataException("There was a problem inserting the Credit Card: " + creditcard.toString());
+
+        return newRowId;
 
     }
 
@@ -74,5 +78,50 @@ public class ExpenseManagerDAO {
         }
 
         return creditCards;
+    }
+
+
+    public long insertExpense(int creditPeriodId, Expense expense) throws CouldNotInsertDataException {
+        SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_FOREIGN_KEY_CREDIT_PERIOD.getName(), new Integer(creditPeriodId));
+        values.put(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_DESCRIPTION.getName(), expense.getDescription());
+        values.put(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_IMAGE.getName(), expense.getImage());
+        values.put(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_AMOUNT.getName(), expense.getAmount().toPlainString());
+        values.put(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_CURRENCY.getName(), expense.getCurrency().getCode());
+        values.put(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_DATE.getName(), expense.getDate().getTimeInMillis());
+        values.put(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_EXPENSE_CATEGORY.getName(), expense.getExpenseCategory().getCode());
+        values.put(ExpenseManagerContract.ExpenseTable.COLUMN_NAME_EXPENSE_TYPE.getName(), expense.getExpenseType().getCode());
+
+
+        long newRowId;
+        newRowId = db.insert(ExpenseManagerContract.ExpenseTable.TABLE_NAME, null, values);
+
+        if(newRowId == -1)
+            throw new CouldNotInsertDataException("There was a problem inserting the Expense: " + expense.toString());
+
+        return newRowId;
+    }
+
+
+    public long insertPayment(int creditPeriodId, Payment payment) throws CouldNotInsertDataException {
+        SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(ExpenseManagerContract.PaymentTable.COLUMN_NAME_FOREIGN_KEY_CREDIT_PERIOD.getName(), new Integer(creditPeriodId));
+        values.put(ExpenseManagerContract.PaymentTable.COLUMN_NAME_DESCRIPTION.getName(), payment.getDescription());
+        values.put(ExpenseManagerContract.PaymentTable.COLUMN_NAME_AMOUNT.getName(), payment.getAmount().toPlainString());
+        values.put(ExpenseManagerContract.PaymentTable.COLUMN_NAME_CURRENCY.getName(), payment.getCurrency().getCode());
+        values.put(ExpenseManagerContract.PaymentTable.COLUMN_NAME_DATE.getName(), payment.getDate().getTimeInMillis());
+
+
+        long newRowId;
+        newRowId = db.insert(ExpenseManagerContract.ExpenseTable.TABLE_NAME, null, values);
+
+        if(newRowId == -1)
+            throw new CouldNotInsertDataException("There was a problem inserting the Payment: " + payment.toString());
+
+        return newRowId;
     }
 }
