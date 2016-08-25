a379c97f47fa13fdd91e9cc637a1fc7ce7f910d2
Alejandro Bicelis <abicelis@gmail.com>
2016-08-24 21:15:55 -0400

    Colors on expenseCategory and expenseType

--- app/src/main/java/ve/com/abicelis/creditcardexpensemanager/enums/ExpenseCategory.java
+++ app/src/main/java/ve/com/abicelis/creditcardexpensemanager/enums/ExpenseCategory.java
@@ -1,27 +1,36 @@
 package ve.com.abicelis.creditcardexpensemanager.enums;
 
+import ve.com.abicelis.creditcardexpensemanager.R;
+
 /**
  * Created by Alex on 6/8/2016.
  */
 public enum ExpenseCategory {
 
-    FOOD("Food"),
-    LEISURE("Leisure"),
-    ENTERTAINMENT("Entertainment"),
-    CLOTHING("Clothing"),
-    EDUCATION("Education");
+    FOOD("Food", R.color.expense_category_1),
+    LEISURE("Leisure", R.color.expense_category_2),
+    ENTERTAINMENT("Entertainment", R.color.expense_category_3),
+    CLOTHING("Clothing", R.color.expense_category_4),
+    EDUCATION("Education", R.color.expense_category_5);
 
 
 
     private String mFriendlyName;
+    private int mColor;
 
-    ExpenseCategory(String friendlyName) {
+    ExpenseCategory(String friendlyName, int color) {
+
         mFriendlyName = friendlyName;
+        mColor = color;
     }
 
 
     public String getCode(){
         return this.name();
+    }
+
+    public int getColor() {
+        return mColor;
     }
 
     public String getFriendlyName() {
--- app/src/main/java/ve/com/abicelis/creditcardexpensemanager/enums/ExpenseType.java
+++ app/src/main/java/ve/com/abicelis/creditcardexpensemanager/enums/ExpenseType.java
@@ -1,19 +1,27 @@
 package ve.com.abicelis.creditcardexpensemanager.enums;
 
+import ve.com.abicelis.creditcardexpensemanager.R;
+
 /**
  * Created by Alex on 6/8/2016.
  */
 public enum ExpenseType {
 
-    ORDINARY("Ordinary"),
-    EXTRAORDINARY("Extraordinary");
+    ORDINARY("Ordinary", "ORD", R.color.expense_category_4),
+    EXTRAORDINARY("Extraordinary", "EXT", R.color.expense_category_7);
+
 
 
 
     private String mFriendlyName;
+    private String mShortName;
+    private int mColor;
 
-    ExpenseType(String friendlyName) {
+    ExpenseType(String friendlyName, String shortName, int color) {
+
         mFriendlyName = friendlyName;
+        mShortName = shortName;
+        mColor = color;
     }
 
 
@@ -23,6 +31,14 @@ public enum ExpenseType {
 
     public String getFriendlyName() {
         return mFriendlyName;
+    }
+
+    public String getShortName() {
+        return mShortName;
+    }
+
+    public int getColor() {
+        return mColor;
     }
 
     @Override
