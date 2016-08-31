bf64a1516e139cc4f8faf6090021791c183e7f54
Alejandro Bicelis <abicelis@gmail.com>
2016-08-31 17:30:43 -0400

    Miscs, colors, strings

---
+++ app/src/main/java/ve/com/abicelis/creditcardexpensemanager/app/utils/Constants.java
@@ -0,0 +1,9 @@
+package ve.com.abicelis.creditcardexpensemanager.app.utils;
+
+/**
+ * Created by Alex on 31/8/2016.
+ */
+public class Constants {
+    public static final String PREFS_NAME = "CCManagerPreferences";
+    public static final String ACTIVE_CC_ID = "ActiveCCId";
+}
