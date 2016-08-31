4be26d5a641bc506d0df43ec345aadff8825441e
Alejandro Bicelis <abicelis@gmail.com>
2016-08-31 17:32:49 -0400

    AddCreditCard activity updated with new creditcard background selector and previewer!

--- app/src/main/java/ve/com/abicelis/creditcardexpensemanager/app/activities/AddCreditCardActivity.java
+++ app/src/main/java/ve/com/abicelis/creditcardexpensemanager/app/activities/AddCreditCardActivity.java
@@ -7,12 +7,19 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
@@ -23,7 +30,10 @@ import java.util.Calendar;
 import java.util.List;
 
 import ve.com.abicelis.creditcardexpensemanager.R;
+import ve.com.abicelis.creditcardexpensemanager.app.adapters.CreditCardAdapter;
+import ve.com.abicelis.creditcardexpensemanager.app.holders.CreditCardViewHolder;
 import ve.com.abicelis.creditcardexpensemanager.database.ExpenseManagerDAO;
+import ve.com.abicelis.creditcardexpensemanager.enums.CreditCardBackground;
 import ve.com.abicelis.creditcardexpensemanager.enums.CreditCardType;
 import ve.com.abicelis.creditcardexpensemanager.enums.Currency;
 import ve.com.abicelis.creditcardexpensemanager.exceptions.CouldNotInsertDataException;
@@ -32,7 +42,7 @@ import ve.com.abicelis.creditcardexpensemanager.model.CreditCard;
 /**
  * Created by Alex on 27/8/2016.
  */
-public class AddCreditCardActivity extends AppCompatActivity implements View.OnClickListener {
+public class AddCreditCardActivity extends AppCompatActivity {
 
     //TAGS
     private static final String CALENDAR_EXPIRATION_TAG = "1";
@@ -43,6 +53,8 @@ public class AddCreditCardActivity extends AppCompatActivity implements View.OnC
     List<Currency> currencies;
     List<CreditCardType> cardTypes;
     List<Integer> days;
+    List<CreditCard> mCreditCardList;
+    CreditCardBackground selectedCreditCardBackground = null;
 
     //UI
     Toolbar toolbar;
@@ -55,6 +67,9 @@ public class AddCreditCardActivity extends AppCompatActivity implements View.OnC
     Spinner cardClosingDay;
     Spinner cardDueDay;
     Button buttonAddCreditCard;
+    RecyclerView mRecyclerView;
+    CreditCardAdapter mAdapter;
+    LinearLayoutManager mLayoutManager;
 
 
     @Override
@@ -71,12 +86,20 @@ public class AddCreditCardActivity extends AppCompatActivity implements View.OnC
         cardType = (Spinner) findViewById(R.id.add_cc_spinner_type);
         cardClosingDay = (Spinner) findViewById(R.id.add_cc_edit_closing);
         cardDueDay = (Spinner) findViewById(R.id.add_cc_edit_due);
+        mRecyclerView = (RecyclerView) findViewById(R.id.add_cc_recycler);
         buttonAddCreditCard = (Button) findViewById(R.id.add_cc_button_add_cc);
-        buttonAddCreditCard.setOnClickListener(this);
+        buttonAddCreditCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNewCardCreation();
+            }
+        });
 
         setUpToolbar();
         setUpPickers();
         setUpSpinners();
+        setUpCCRecyclerView();
+        setUpRecyclerUpdater();
 
         //Check if intent comes with cameFromWelcomeScreen = true;
         cameFromWelcomeScreen = true;
@@ -150,13 +173,119 @@ public class AddCreditCardActivity extends AppCompatActivity implements View.OnC
         cardDueDay.setAdapter(dueDayAdapter);
     }
 
-    @Override
-    public void onClick(View view) {
-        int i = view.getId();
+    private void setUpCCRecyclerView() {
 
-        if(i == R.id.add_cc_button_add_cc) {
-            handleNewCardCreation();
-        }
+        mCreditCardList = CreditCard.getCreditCardBackgroundTypesList(this);
+        CreditCardViewHolder.CreditCardSelectedListener ccSelectedListener = new CreditCardViewHolder.CreditCardSelectedListener() {
+            @Override
+            public void OnCreditCardSelected(CreditCard creditCard) {
+                Toast.makeText(AddCreditCardActivity.this, "Background selected", Toast.LENGTH_SHORT).show();
+                selectedCreditCardBackground = creditCard.getCreditCardBackground();
+            }
+        };
+
+        mAdapter = new CreditCardAdapter(getApplicationContext(), mCreditCardList, ccSelectedListener);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+    }
+
+
+    private void setUpRecyclerUpdater() {
+
+        cardBankName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                for (CreditCard c : mCreditCardList) {
+                    c.setBankName(cardBankName.getText().toString());
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+
+        cardAlias.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                for (CreditCard c : mCreditCardList) {
+                    c.setCardAlias(cardAlias.getText().toString());
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+
+        cardNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                for (CreditCard c : mCreditCardList) {
+                    c.setCardNumber(cardNumber.getText().toString());
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+
+        cardExpiration.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                for (CreditCard c : mCreditCardList) {
+                    c.setCardExpiration(cardExpirationCal);
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+
+
+        cardCurrency.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                for (CreditCard c : mCreditCardList) {
+                    c.setCurrency(currencies.get(cardCurrency.getSelectedItemPosition()));
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+
+        cardType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                for (CreditCard c : mCreditCardList) {
+                    c.setCardType(cardTypes.get(cardType.getSelectedItemPosition()));
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+
     }
 
     @Override
@@ -195,6 +324,11 @@ public class AddCreditCardActivity extends AppCompatActivity implements View.OnC
             return;
         }
 
+        if(selectedCreditCardBackground == null) {
+            Toast.makeText(this, getResources().getString(R.string.activity_add_new_cc_error_cc_background_not_selected), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         String bankName = cardBankName.getText().toString();
         String number = cardNumber.getText().toString();
         Currency currency = currencies.get(cardCurrency.getSelectedItemPosition());
@@ -203,7 +337,7 @@ public class AddCreditCardActivity extends AppCompatActivity implements View.OnC
 
         ExpenseManagerDAO dao = new ExpenseManagerDAO(this);
         try {
-            dao.insertCreditCard(new CreditCard(alias, bankName, number, currency, type, cardExpirationCal, closing, due));
+            dao.insertCreditCard(new CreditCard(alias, bankName, number, currency, type, cardExpirationCal, closing, due, selectedCreditCardBackground));
         }catch(CouldNotInsertDataException e) {
             Toast.makeText(this, "There was a problem inserting the credit card!", Toast.LENGTH_SHORT).show();
         }
