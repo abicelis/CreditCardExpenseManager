79ff9a7a598da66511e6f6e31980b3c972caf90f
Alejandro Bicelis <abicelis@gmail.com>
2016-09-04 09:10:34 -0400

    LauncherActivity sanity checks

--- app/src/main/java/ve/com/abicelis/creditcardexpensemanager/app/activities/LauncherActivity.java
+++ app/src/main/java/ve/com/abicelis/creditcardexpensemanager/app/activities/LauncherActivity.java
@@ -4,9 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import ve.com.abicelis.creditcardexpensemanager.app.utils.Constants;
+import ve.com.abicelis.creditcardexpensemanager.app.utils.SharedPreferencesUtils;
 import ve.com.abicelis.creditcardexpensemanager.database.ExpenseManagerDAO;
+import ve.com.abicelis.creditcardexpensemanager.exceptions.CreditCardNotFoundException;
+import ve.com.abicelis.creditcardexpensemanager.exceptions.CreditPeriodNotFoundException;
+import ve.com.abicelis.creditcardexpensemanager.exceptions.SharedPreferenceNotFoundException;
+import ve.com.abicelis.creditcardexpensemanager.model.CreditCard;
 
 /**
  * Created by Alex on 26/8/2016.
@@ -17,15 +23,32 @@ public class LauncherActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ExpenseManagerDAO dao = new ExpenseManagerDAO(this.getApplicationContext());
-        Intent intent;
+        ExpenseManagerDAO mDao = new ExpenseManagerDAO(getApplicationContext());
 
-        //if(dao.getCreditCardList().size() == 0)
-            intent = new Intent(this, WelcomeActivity.class);
-         //else
-         //   intent = new Intent(this, HomeActivity.class);
+        //Check if ACTIVE_CC_ID exists and it is valid
+        try {
+            int activeCreditCardId = SharedPreferencesUtils.getInt(getApplicationContext(), Constants.ACTIVE_CC_ID);
+            mDao.getCreditCard(activeCreditCardId);
+        }catch(SharedPreferenceNotFoundException | CreditCardNotFoundException e) {
 
-        startActivity(intent);
+            //if there's at least one CreditCard in database, set it's id to ACTIVE_CC_ID
+            if(mDao.getCreditCardList().size() > 0) {
+                SharedPreferencesUtils.setInt(getApplicationContext(), Constants.ACTIVE_CC_ID,  mDao.getCreditCardList().get(0).getId());
+            } else {
+                //There are no credit cards in the system, send to welcomeActivity
+                startActivity(new Intent(this, WelcomeActivity.class));
+                finish();
+                return;
+            }
+        }
+
+        //TODO: maybe here's the place to check if new CreditPeriods need to be created?
+
+
+        //If all is good, go home!
+        startActivity(new Intent(this, WelcomeActivity.class));
+        ///startActivity(new Intent(this, HomeActivity.class));
         finish();
     }
+
 }
