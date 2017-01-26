a38a4061f794445bb3e88a661b480cc2f041763b
abicelis <abicelis@gmail.com>
2017-01-25 22:50:12 -0400

    Housekeeping - HomeActivity

--- app/src/main/java/ve/com/abicelis/creditcardexpensemanager/app/activities/HomeActivity.java
+++ app/src/main/java/ve/com/abicelis/creditcardexpensemanager/app/activities/HomeActivity.java
@@ -5,11 +5,9 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import ve.com.abicelis.creditcardexpensemanager.R;
 import ve.com.abicelis.creditcardexpensemanager.app.fragments.NavigationDrawerFragment;
-import ve.com.abicelis.creditcardexpensemanager.app.fragments.ExpenseListFragment;
 import ve.com.abicelis.creditcardexpensemanager.app.fragments.OverviewFragment;
 
 public class HomeActivity extends AppCompatActivity {
@@ -37,7 +35,6 @@ public class HomeActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.home_toolbar);
         DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-
         //Set toolbar as actionbar
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -51,30 +48,6 @@ public class HomeActivity extends AppCompatActivity {
         //Load the NavigationDrawerFragment, pass the drawerLayout
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.nav_drawer_fragment);
         drawerFragment.setDrawerLayout(mDrawerLayout);
-
-
-//        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) rootView.findViewById(R.id.home_collapsing);
-//        AppBarLayout appBarLayout = (AppBarLayout) rootView.findViewById(R.id.home_appbar);
-//        mToolbar = (Toolbar) rootView.findViewById(R.id.home_toolbar);
-//        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-//            boolean isShow = false;
-//            int scrollRange = -1;
-//
-//            @Override
-//            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-//                if (scrollRange == -1) {
-//                    scrollRange = appBarLayout.getTotalScrollRange();
-//                }
-//                if (scrollRange + verticalOffset == 0) {
-//                    collapsingToolbarLayout.setTitle(getResources().getString(R.string.app_name));
-//                    isShow = true;
-//                } else if(isShow) {
-//                    collapsingToolbarLayout.setTitle(" ");//carefull there should a space between double quote otherwise it wont work
-//
-//                    isShow = false;
-//                }
-//            }
-//        });
     }
 
 }
